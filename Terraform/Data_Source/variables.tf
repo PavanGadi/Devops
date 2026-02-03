70725b7538d724dfbfcc1bfcb384d7d0773e741e
PavanGadi <gpavan777@gmail.com>
2026-02-03 21:34:23 +0530

    Added Data Source

---
+++ Terraform/Data_Source/variables.tf
@@ -0,0 +1,34 @@
+variable "instance_type" {
+    default = "t2.micro"
+    type = string
+}
+
+variable "tags" {
+    default = {
+        Project = "Expense"
+        Environment = "Dev"
+        Module = "DB"
+        Name = "DB"
+    }
+}
+
+variable "sg_name" {
+    default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "allowing port 22"
+}
+
+variable "ssh_port" {
+    default = 22
+}
+
+variable "protocol" {
+    default = "tcp"
+}
+
+variable "allowed_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
